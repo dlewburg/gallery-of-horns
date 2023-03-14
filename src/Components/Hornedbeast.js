f5999e1b92162d97fa70a27f63fb315b471696c9
Dasha Burgos <dasha.lewis@gmail.com>
2023-03-14 19:26:14 -0400

    added state to component

--- src/Components/Hornedbeast.js
+++ src/Components/Hornedbeast.js
@@ -2,19 +2,29 @@ import React, { Component } from 'react'
 import '../css/Hornedbeast.css'
 
 export default class Hornedbeast extends Component {
+  constructor(props){
+    super(props);
+    
+    this.state = {
+      favorite: 0
+    }
+  }
+
+  // Method to update state for each beast to track likes
+  
   render() {
     return (
-      <section>
-        <h2>{this.props.title}</h2>
+    <section>
+      <h2>{this.props.title}</h2>
 
-        <img src={this.props.image_url}
-          alt={this.props.description}
-          title={this.props.title} />
+      <img src={this.props.image_url}
+        alt={this.props.description}
+        title={this.props.title} />
 
-        <p>{this.props.description}</p>
-        <p>❣️Favorite❣️</p>
-        <p>Choose Favorite</p>
-      </section>
+      <p>{this.props.description}</p>
+      <p>❣️Favorite {this.state.favorite}❣️</p>
+      <p>Choose As Favorite</p>
+    </section>
     )
   }
 }
