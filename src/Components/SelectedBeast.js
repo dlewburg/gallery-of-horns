017d537482f28fa49018557666f9dae79d3f392b
Dasha Burgos <dasha.lewis@gmail.com>
2023-03-15 21:51:28 -0400

    styled modal card

--- src/Components/SelectedBeast.js
+++ src/Components/SelectedBeast.js
@@ -11,11 +11,11 @@ export default class SelectedBeast extends Component {
     return (
       
       <Modal show = {this.props.display} onHide={this.props.onHide}>
-        <Card>
+        <Card bg="success" border="warning" style={{ width: '3 rem'}}>
           <Card.Title>{this.props.selectedBeast.title}</Card.Title>
           <Card.Img src={this.props.selectedBeast.image_url}></Card.Img>
           <Card.Body>
-            <Card.Text>{this.props.selectedBeast.description}</Card.Text>
+            <Card.Text text="warning">{this.props.selectedBeast.description}</Card.Text>
           </Card.Body>
         </Card>
         <Modal.Footer><Button onClick={this.props.onHide}>Close</Button></Modal.Footer>
